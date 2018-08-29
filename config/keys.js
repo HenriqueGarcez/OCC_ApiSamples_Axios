9a944b7b48281c3dcc4b0b459283654be1c0bf07
HenriqueGarcez <30879178+HenriqueGarcez@users.noreply.github.com>
2018-08-29 16:08:47 -0300

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -3,17 +3,17 @@ module.exports = {
     apiKey: () => {
     function apiKey(){        
     }
-    apiKey.key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI2ZWVhZWM3MC1hMmFlLTRmZTItYjA2MC1kOTAwNjljZGI4MTciLCJpc3MiOiJhcHBsaWNhdGlvbkF1dGgiLCJleHAiOjE1NjY0MjIzMDIsImlhdCI6MTUzNDg4NjMwMn0=.r7qI/Tw2RTDOxu5bz9NSbjvBhx7FjCewqHo50VeuS+c=';
-    apiKey.ID = '6eeaec70-a2ae-4fe2-b060-d90069cdb817';
+    apiKey.key = '';
+    apiKey.ID = '';
     return apiKey;
 },
     adminKey: () => {
     function adminKey(){        
     }
-    adminKey.user = 'henrique.garcez@compasso.com.br';
-    adminKey.password = 'C0nnect123';
-    adminKey.ID = '123456';
+    adminKey.user = '';
+    adminKey.password = '';
+    adminKey.ID = '';
     return adminKey;
 },
-    host: 'https://ccadmin-test-zdda.oracleoutsourcing.com'
+    host: 'https://ccadmin.oracleoutsourcing.com'
 }
